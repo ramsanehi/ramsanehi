fceffecb569dfb1d3979bac3b3be71eb0d0c6d38
ramsanehi <ramsanehi1993@gmail.com>
2016-06-11 11:19:35 +0530

    module

---
+++ mymodule.js
@@ -0,0 +1,10 @@
+var sayHello=function(){
+	console.log('hello How R U !');
+
+}
+var sayGoodbye=function() {
+	console.log('Goodbye');
+}
+
+exports.sayHello = sayHello;
+exports.sayGoodbye = sayGoodbye;
