6aabcbeb48f87dfe1e663cb8e5139d8d35ee880e
ramsanehi <ramsanehi1993@gmail.com>
2016-06-11 09:21:15 +0530

    path

---
+++ path.js
@@ -0,0 +1,11 @@
+var http = require('http')
+var url = require('url');
+var server = http.createServer(function(req,res){
+	var page=url.parse(req.url).pathname;
+	console.log(page);
+	res.writeHead(200,{"contentType":"text/html"})
+	 res.write('well hello'+page);	 	
+	 res.end();
+	 });
+
+	 server.listen(8082);
