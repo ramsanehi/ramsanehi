94c3bdd785fbc48142267e48acb18837d51e57a9
ramsanehi <ramsanehi1993@gmail.com>
2016-06-12 10:49:17 +0530

    12

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var express =require('express')
 var app=express()
 
 app.get('/loginup',function(req,res){
-res.render('loginup.ejs')
+	var array1=["red","blue","green"]
+res.render('loginup.ejs',{
 
-
-res.end()
-})
+data:array1
+});
+});
 app.listen(8081)
 console.log("hello ram");
